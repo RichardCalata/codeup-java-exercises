fa8b20a1dbac617de414c7fe2dd8ab69cdb7d6b3
richardcalata <calatar6@gmail.com>
2018-05-02 08:21:49 -0500

    Methods Exercise Sum

---
+++ src/MethodsExercises.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class MethodsExercises {
+public static void main(String[]args){
+    Scanner userInput = new Scanner(System.in);
+    System.out.print("Enter a Number: ");
+    int oneNumber = userInput.nextInt();
+    System.out.print("Enter another number: ");
+    int anotherNumber =userInput.nextInt();
+    int sum = oneNumber + anotherNumber;
+    System.out.println("the sum of " + oneNumber + " and " + anotherNumber + " is " + sum + ".");
+
+
+
+            }
+        }
