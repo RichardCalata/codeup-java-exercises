0a696bf4dcfda9b09b3b22e053021f3daf968dc8
richardcalata <calatar6@gmail.com>
2018-05-02 12:22:59 -0500

    Methods Exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,16 +2,67 @@
 import java.util.Scanner;
 
 public class MethodsExercises {
-public static void main(String[]args){
-    Scanner userInput = new Scanner(System.in);
-    System.out.print("Enter a Number: ");
-    int oneNumber = userInput.nextInt();
-    System.out.print("Enter another number: ");
-    int anotherNumber =userInput.nextInt();
-    int sum = oneNumber + anotherNumber;
-    System.out.println("the sum of " + oneNumber + " and " + anotherNumber + " is " + sum + ".");
 
+
+public static void main(String[]args){
+
+    Scanner numeral = new Scanner(System.in);
+
+    System.out.println("Enter a number: ");
+    double number1 = numeral.nextDouble();
+    System.out.println("Enter another Number");
+    double number2 = numeral.nextDouble();
+
+    double sum = addition(number1,number2);
+    System.out.println("the Sum of " + number1 + " and " + number2 + " is " + sum);
+    double diff = subtraction(number1,number2);
+    System.out.println("the Difference between " + number1 + " and " + number2 + " is " + diff);
+    double product = multiply(number1,number2);
+    System.out.println("the Product of " + number1 + " and " + number2 + " is " + product);
+    double quotient = divide(number1,number2);
+    System.out.println("the Quotient of " + number1 + " divided by " + number2 + " is " + quotient);
+    double modu = modulus(number1,number2);
+    System.out.println("the Remainder of " + number1 + " divided by " + number2 + " is " + modu);
+
+
+    System.out.print("Enter a number between 1 and 10: ");
+    int userInput = getInteger(1, 10);
+    System.out.println(userInput + " IS a number between 1 and 10");
 
 
             }
+
+    public static double addition(double a, double b){
+
+             return a+b;
+    }
+
+    public static double subtraction(double a, double b) {
+
+        return a - b;
+    }
+
+    public static double multiply(double a, double b){
+
+        return a*b;}
+
+    public static double divide(double a, double b){
+
+        return a/b;}
+
+    public static double modulus(double a, double b){
+
+        return a%b;
         }
+
+    public static int getInteger(int min, int max){
+        System.out.println("Please input an integer between " + min + " and " + max + ":");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        if(number < min || number > max) {
+            return getInteger(min, max);
+        } else {
+            return number;
+        }
+        }
+    }
