2db02eccaa0d4dffdb9bc17896d88c7fa71f6ecb
richardcalata <calatar6@gmail.com>
2018-05-04 12:15:38 -0500

    add empty methods to input

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,6 +1,28 @@
 package util;
 
+import java.util.Scanner;
+
 public class Input {
+        //privat property names scanner
+    private Scanner scanner;  //=null
+
+    // 2 options: assign variable directly(inline) -- or constructor --
+    public Input() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String getString() {
+        return null;
+    }
+
+    public boolean yesNo(){
+        return true;
+    }
+
+    public int getInt(){
+        return 0;
+    }
+
 
 
 }
