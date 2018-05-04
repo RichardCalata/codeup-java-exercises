6a6655125abe5ae48b6a13af4bd3ad70ab4f05a0
richardcalata <calatar6@gmail.com>
2018-05-04 13:59:28 -0500

    create constructor to initialize value of radius

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,4 +1,15 @@
 package shapes;
 
 public class Circle {
+
+    private double radius;
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public Circle(double radius)
+    public double getArea()
+    public double getCircumference()
+
 }
